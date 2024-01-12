788e46ac2f80196046e9ac09e957d5ddb1efc92c
danferns <dannywritescode@gmail.com>
2024-01-12 16:26:17 +0530

    fix stuck notes when playing chords

--- src/chord_player.py
+++ src/chord_player.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, Lock
 import time
 
 from setup_daw import note_on, note_off
@@ -47,31 +47,52 @@ def chord_to_notes(root, shape):
 
 
 chords_held = []
+notes_held = {}  # note -> chord[]
 
-def strum_chord(notes, velocity):
-    notes.sort() # strum up
-    delay = params["strum"] / 127
-    delay *= 5 # max duration
+hold_release_lock = Lock()
+
+def hold_note_of_chord(note, root, shape):
+    hold_release_lock.acquire()
+    if {"root": root, "shape": shape} in chords_held:
+        if note not in notes_held:
+            notes_held[note] = []
+        notes_held[note].append({"root": root, "shape": shape})
+        note_on(note, params["velocity"])
+    hold_release_lock.release()
+
+
+def release_note_of_chord(note, root, shape):
+    hold_release_lock.acquire()
+    if note in notes_held:
+        if {"root": root, "shape": shape} in notes_held[note]:
+            notes_held[note].remove({"root": root, "shape": shape})
+            if len(notes_held[note]) == 0:
+                note_off(note)
+    
+    hold_release_lock.release()
+
+
+def delay_time():
+    return params["strum"] / 127 * 5
+
+def strum_chord(notes, root, shape):
+    notes.sort()  # strum up
     for note in notes:
-        note_on(note, velocity)
-        time.sleep(delay)
+        # this will internally check if the chord is still held
+        hold_note_of_chord(note, root, shape)
+        # computing delay here means we use the realtime knob value
+        time.sleep(delay_time())
+
 
 def play_chord(root, shape):
     notes = chord_to_notes(root, shape)
-    strum_thread = Thread(target=strum_chord, args=(notes, params["velocity"]))
+    strum_thread = Thread(target=strum_chord, args=(notes, root, shape))
     strum_thread.start()
     chords_held.append({"root": root, "shape": shape})
 
 
 def release_chord(root, shape):
-    this_chord_notes = set(chord_to_notes(root, shape))
-    other_chords_notes = set()
+    chord_notes = set(chord_to_notes(root, shape))
+    for note in chord_notes:
+        release_note_of_chord(note, root, shape)
     chords_held.remove({"root": root, "shape": shape})
-    for chord in chords_held:
-        other_chords_notes.update(chord_to_notes(chord["root"], chord["shape"]))
-
-    # only release notes that are not held down by other chords simultaneously
-    notes_to_release = this_chord_notes - other_chords_notes
-
-    for note in notes_to_release:
-        note_off(note)
